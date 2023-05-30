73d09e0ee54424f2b36f11e947cd043c58bafdd2
Luna Geens <s0192441@ad.ua.ac.be>
2023-05-30 16:03:35 +0200

    Changed file type READ ME 2

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -1,18 +1,27 @@
 package runners;
 
-// When Extent reports are implemented: import com.vimalselvam.cucumber.listener.Reporter;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+// TODO Change name feature file, README file, in runner class, and where else is needed
 
-// TODO Implement Extent reports and alter this class.
-// TODO BrowserStack? How with Config file?
+// TODO Menu items should be loaded, not all courses. Check that in Exercise 6 with script!!!
+// TODO Maybe remove the output For each course and professor? How can we see output in the reports?
+// TODO Warning when the assignment link does not exists in ex 7, no error. Check this
+// TODO Explanation input and output Exercise 7 in README?
+// TODO Change given step in @Exercise8 - only need name and no group number, need two dates as well (?)
+
+// TODO Make different step file for Exercise 6, 7 and 8. Give it appropriate name and adjust this here. Also in runner.
+// TODO TestContext for different step files?
+
+
+// TODO Implement Allure reports and alter Runner class. Add explanation in README. Deploy to Netlify.
+// TODO BrowserStack testing? How with Config file?
+
 /**
  * JUnit class that specifies how Cucumber should run tests.
  */
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features", //specify the .feature file
@@ -26,14 +35,9 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber-reports/Cucumber.json", // generates same information in JSon format, to be process by other tools
                 "junit:target/cucumber-reports/Cucumber.xml" // generates junit reports
         })
-// For Extent reports: plugin= {..., "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/AdvancedReport.html"}
+
 public class RunnerTest {
 
     // this may be empty
 
-    //For Extent Reports:
-    // @AfterClass
-    // public static void writeExtentReport(){
-    // Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
-    //}
 }
